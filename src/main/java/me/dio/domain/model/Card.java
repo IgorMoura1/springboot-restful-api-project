10c4366c2440e2590e5f9f1d0c3eef009ef8dbf8
IgorMoura1 <igor.moura@sptech.school>
2024-08-03 21:42:16 -0300

    Hotfix nas entidades precision e scale

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(unique = true, nullable = false, length = 20)
     private String number;
 
-    @Column(name ="available_limit",nullable = false, scale = 13, precision = 2)
+    @Column(name ="available_limit",nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
 
